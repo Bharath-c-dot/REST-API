ae46f66f349a49d6e0eaa4aad2f6916c7ae32c0c
Gon-ai <bharathilucartheh@gmail.com>
2026-07-28 19:05:30 +0530

    used useEffect to fetch

--- src/Rest.jsx
+++ src/Rest.jsx
@@ -4,17 +4,27 @@ const Rest = () => {
 
     const [data,setdata] = useState([])
 
-    async function fetching(){
-
-        const mydata = await fetch("https://jsonplaceholder.typicode.com/users")
-        const fetchdata = await mydata.json();
-        setdata(fetchdata)
-        console.log(fetchdata)
-    }
-
     useEffect(()=>{
-        fetching()
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then((fetchdata)=> fetchdata.json())
+        .then((data)=>{
+            setdata(data)
+            console.log(data)
+        })
+        
     },[])
+
+    // async function fetching(){
+
+    //     const mydata = await fetch("https://jsonplaceholder.typicode.com/users")
+    //     const fetchdata = await mydata.json();
+    //     setdata(fetchdata)
+    //     console.log(fetchdata)
+    // }
+
+    // useEffect(()=>{
+    //     fetching()
+    // },[])
 
     console.log(data)
 
